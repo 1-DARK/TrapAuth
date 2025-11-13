22a6593d51fb79facc1dff76aa276eb29930ecda
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-13 22:21:45 +0530

    add signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,4 +1,5 @@
 import { User } from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
 
 export const signup = async (req, res) => {
   const { email, password, name } = req.body;
@@ -6,9 +7,26 @@ export const signup = async (req, res) => {
     if (!email || !password || !name) {
       throw new Error("All fields are required");
     }
-
     const userAlreadyExists = await User.findOne({ email });
-  } catch (error) {}
+    if (userAlreadyExists) {
+      return res
+        .status(400)
+        .json({ success: false, message: "User Already Exists" });
+    }
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    const verificationToken = Math.floor(
+      100000 + Math.random() * 900000
+    ).toString();
+    const user = new User({
+      email,
+      password: hashedPassword,
+      name,
+      verificationToken,
+      verificationTokenExpiresAt: Date.now() + 24 * 60 * 60 * 1000,
+    });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
 };
 
 export const login = async (req, res) => {
