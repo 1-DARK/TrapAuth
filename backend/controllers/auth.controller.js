34f0ececf55ee2e193567ac72a2ca7833e2543fe
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-15 23:03:56 +0530

    init verifyemail function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -44,6 +44,25 @@ export const signup = async (req, res) => {
   }
 };
 
+export const verifyEmail = async (req, res) => {
+  const { code } = req.body;
+  try {
+    const user = await User.findOne({
+      verificationToken: code,
+      verificationTokenExpiresAt: { $gt: Date.now() },
+    });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({
+          success: false,
+          message: "Invalid or expired verification code",
+        });
+    }
+  } catch (error) {}
+};
+
 export const login = async (req, res) => {
   res.send("login");
 };
