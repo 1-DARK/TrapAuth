d5dc14badaa336d0b6273bd90e8fd1484bc6073b
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-16 23:51:03 +0530

    init

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -107,6 +107,8 @@ export const login = async (req, res) => {
   }
 };
 
+export const forgotPassword = async () => {};
+
 export const logout = async (req, res) => {
   res.clearCookie("token");
   res.status(200).json({ success: true, message: "Logged out successfully" });
