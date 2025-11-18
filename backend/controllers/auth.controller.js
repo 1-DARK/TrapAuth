cc14123d2753c81c7a6c7bb7dd6da63d60aa9c68
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-18 22:07:04 +0530

    update reset pass function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -151,4 +151,25 @@ export const forgotPassword = async (req, res) => {
   }
 };
 
-export const resetPassword = async (req, res) => {};
+export const resetPassword = async (req, res) => {
+  try {
+    const { token } = req.params;
+    const { password } = req.body;
+    const user = await User.findOne({
+      resetPasswordToken: token,
+      resetPasswordExpiresAt: { $gt: Date.now() },
+    });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid or expired reset token" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 10);
+    user.password = hashedPassword;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpiresAt = undefined;
+
+    await user.save();
+  } catch (error) {}
+};
