93dfb00208d34b2392720a1f8349bae30c60879f
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-20 23:04:37 +0530

    add new pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,8 @@
 import FloatingShape from "./components/FloatingShape";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
@@ -24,6 +28,11 @@ function App() {
         left="-10%"
         delay={2}
       />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/login" element={<LoginPage />} />
+      </Routes>
     </div>
   );
 }
