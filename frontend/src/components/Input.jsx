67c38bdccb7227979318c1851787b0a3a9fd1e66
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-23 19:51:09 +0530

    init

---
+++ frontend/src/components/Input.jsx
@@ -0,0 +1,5 @@
+const Input = ({ icon: Icon, ...props }) => {
+  return <div className="relative mb-6">Input</div>;
+};
+
+export default Input;
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,7 @@
 import { motion } from "framer-motion";
 import { useState } from "react";
+import Input from "../components/Input";
+import { Loader, Lock, Mail, User } from "lucide-react";
 const SignUpPage = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
@@ -17,7 +19,7 @@ const SignUpPage = () => {
 			overflow-hidden"
     >
       <div className="p-8">
-        <h2 className="text-3xl font-bold mb-6 text-center bg-gradient-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
+        <h2 className="text-3xl font-bold mb-6 text-center bg-linear-to-r from-green-400 to-emerald-500 text-transparent bg-clip-text">
           Create Account
         </h2>
         <form onSubmit={handleSignUp}>
@@ -46,7 +48,7 @@ const SignUpPage = () => {
           <PasswordStrengthMeter password={password} />
 
           <motion.button
-            className="mt-5 w-full py-3 px-4 bg-gradient-to-r from-green-500 to-emerald-600 text-white 
+            className="mt-5 w-full py-3 px-4 bg-linear-to-r from-green-500 to-emerald-600 text-white 
 						font-bold rounded-lg shadow-lg hover:from-green-600
 						hover:to-emerald-700 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2
 						 focus:ring-offset-gray-900 transition duration-200"
