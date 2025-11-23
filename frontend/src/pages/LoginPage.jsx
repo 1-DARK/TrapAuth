820e7a9cae552aee112615e3c5989bc0ce0c7efd
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-11-23 22:24:39 +0530

    init functions

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,5 +1,18 @@
+import { useState } from "react";
+import { motion } from "framer-motion";
+import { Mail, Lock, Loader } from "lucide-react";
+import { Link } from "react-router-dom";
+
 const LoginPage = () => {
-  return <div>login page</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setEmail("");
+    setPassword("");
+  };
+  return <div></div>;
 };
 
 export default LoginPage;
