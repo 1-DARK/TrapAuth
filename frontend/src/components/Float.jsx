dd201d1f6da1e672926384ba29c3ca2fd9ec1f46
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-12-25 23:02:36 +0530

    some changes

--- frontend/src/components/Float.jsx
+++ frontend/src/components/Float.jsx
@@ -4,11 +4,6 @@ const FloatingShape = ({ color, size, top, left, delay }) => {
     <motion.div
       className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl`}
       style={{ top, left }}
-      animate={{
-        y: ["0%", "100%", "0%"],
-        x: ["0%", "100%", "0%"],
-        rotate: [0, 360],
-      }}
       transition={{
         duration: 20,
         ease: "linear",
