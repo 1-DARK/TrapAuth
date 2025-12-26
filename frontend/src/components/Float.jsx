8e85a584176745ec507f6d37d7e88d6dd3e37ac8
Harsh Vardhan Chauhan <harshvardhanchauhan776@gmail.com>
2025-12-26 21:07:08 +0530

    init

--- frontend/src/components/Float.jsx
+++ frontend/src/components/Float.jsx
@@ -1,18 +1,14 @@
-import { motion } from "framer-motion";
-const FloatingShape = ({ color, size, top, left, delay }) => {
+const Hl = ({ icon: Icon, ...props }) => {
   return (
-    <motion.div
-      className={`absolute rounded-full ${color} ${size} opacity-20 blur-xl`}
-      style={{ top, left }}
-      transition={{
-        duration: 20,
-        ease: "linear",
-        repeat: Infinity,
-        delay,
-      }}
-      aria-hidden="true"
-    />
+    <div className="relative mb-6">
+      <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+        <Icon className="size-5 text-green-500" />
+      </div>
+      <input
+        {...props}
+        className="w-full pl-10 pr-3 py-2 bg-gray-800 bg-opacity-50 rounded-lg border border-gray-700 focus:border-green-500 focus:ring-2 focus:ring-green-500 text-white placeholder-gray-400 transition duration-200"
+      />
+    </div>
   );
 };
-
-export default FloatingShape;
+export default Input;
